da779d061b05cdc6a8f93141a78ffc8e6ed43b13
Ghost Dev <ntrongquy2003@gmail.com>
2024-08-12 18:05:47 +0700

    Dom event

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -4,16 +4,31 @@ import React from "react";
 class MyComponents extends React.Component{
   //.jsx
   // Một render() thì chỉ chứa được 1 element
+  // state la 1 object
   state = {
     name: 'quy',
     age : 21,
     address: 'hatinh'
   };
+  handlerClick (event){
+    console.log('>>> Hello ban from react');
+    // console.log(event);
+    console.log('my name is ',this.state.name)
+  }
+
+  handlerOnMoverOver(event){
+    // console.log(event)
+    // console.log(event.target)
+    console.log(event.pageX)
+  }
+
   render(){
     // .jsx
     return (
       <div>
         my name is {this.state.name} and i'm from country {this.state.address}
+        <button onMouseOver={this.handlerOnMoverOver}>Hold me</button>
+        <button onClick={this.handlerClick}>Click me</button>
       </div>
       
     );
