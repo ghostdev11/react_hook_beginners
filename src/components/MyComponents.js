b80d5c64483b8aa68eb009fe39c420fdf13e646f
Ghost Dev <ntrongquy2003@gmail.com>
2024-08-12 23:45:57 +0700

    props

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -3,12 +3,17 @@
 import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 class MyComponents extends React.Component{
   render(){
-    // .jsx
+    const MyInfor = ['ac', 'c' , 'v'];
     return (
       <div>
         <UserInfor/>
+        <br/> <br/>
+        <DisplayInfor name='Diablo' age= {33}/>
+        <hr/>
+        <DisplayInfor name={'Rimuru'} age= {12} MyInfor={MyInfor}/>
       </div>
     );
   }
