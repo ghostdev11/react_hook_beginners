a3c2bbc9a7dd4acabfce9e3358c77d5322b255fe
Ghost Dev <ntrongquy2003@gmail.com>
2024-08-14 22:27:09 +0700

    useState Hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
-// stateless(viết bằng class) - stateful(viết bằng function components) 
+// stateless(viết bằng class) - stateful(viết bằng function components)
 // class DisplayInfor extends React.Component {
 //   render() {
 //     console.log(">>check call render");
@@ -37,9 +37,28 @@ import logo from "./../logo.svg";
 // }
 const DisplayInfor = (props) => {
   const { listUsers } = props;
+  // in function component no function setState is update biến state
+  //  const [isNameState(Name state), setNameState(công cụ cập nhật State)] = useState();
+  const [isShowHideListUser, setShowHideListUser] = useState(true);
+
+  // định nghĩa 1 function
+  const handlerShowHideListUser = () => {
+    setShowHideListUser(!isShowHideListUser);
+  };
   return (
     <div className="display-infor-container">
-      {true && (
+      <div>
+        <span
+          onClick={() => {
+            handlerShowHideListUser();
+          }}
+        >
+          {
+            isShowHideListUser === true ? "Hide ListUser" : "Show ListUser"
+          }
+        </span>
+      </div>
+      {isShowHideListUser && (
         <>
           {listUsers.map((user, index) => {
             return (
