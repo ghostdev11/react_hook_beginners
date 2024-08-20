947bd0df4068665eb44176769f5f983166a618d2
Ghost Dev <ntrongquy2003@gmail.com>
2024-08-20 12:16:53 +0700

    setup bostrap/react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponents from "./components/MyComponents";
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello word from to React
-        <MyComponents></MyComponents>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div className="App">hello word</div>;
+};
 export default App;
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello word from to React
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,40 +1,8 @@
-import React, { useState } from "react";
+import React, {useEffect, useState } from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 // stateless(viết bằng class) - stateful(viết bằng function components)
-// class DisplayInfor extends React.Component {
-//   render() {
-//     console.log(">>check call render");
-//     const { listUsers } = this.props;
-//     // console.log(listUsers);
-//     return (
-//       <div className="display-infor-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user, index) => {
-//               return (
-//                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                   <div>
-//                     <div>My name's {user.name}</div>
-//                     <div>My age's {user.age}</div>
-//                   </div>
-//                   <div>
-//                     <button
-//                       onClick={() => this.props.handlerDeleteUser(user.id)}
-//                     >
-//                       Delete
-//                     </button>
-//                   </div>
-//                   <hr />
-//                 </div>
-//               );
-//             })}
-//           </>
-//         )}
-//       </div>
-//     );
-//   }
-// }
+
 const DisplayInfor = (props) => {
   const { listUsers } = props;
   // in function component no function setState is update biến state
@@ -45,6 +13,15 @@ const DisplayInfor = (props) => {
   const handlerShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser);
   };
+  console.log('>>>check call me render')
+  useEffect(
+    ()=>{
+    if(listUsers.length === 0){
+      alert('User it not >1')
+    }
+    console.log('>>>>Check call me useEffect')
+  }, [listUsers]
+);
   return (
     <div className="display-infor-container">
       <div>
