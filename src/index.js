1a8782b46c67d8b6fe1da47703c78b38eb29c48a
Ghost Dev <ntrongquy2003@gmail.com>
2024-08-26 02:08:51 +0700

    next page with Link

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import 'bootstrap/dist/css/bootstrap.min.css';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
-      <App />
+    <BrowserRouter>
+      <Routes>
+        {/*  đang code trang home page khi vào trang 
+        -> render 2 button */}
+        <Route path="/" element={<App />} />
+        <Route path="users" element={<User />} />
+        <Route path="admins" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
     {/* </React.StrictMode> */}
   </Provider>
 );
